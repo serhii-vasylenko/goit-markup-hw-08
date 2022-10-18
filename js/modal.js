acd2f34d3806ef089099328d0690ec473b962428
Jerrssey <fankton@gmail.com>
2022-10-18 15:21:31 +0300

    minor fixes

--- js/modal.js
+++ js/modal.js
@@ -4,17 +4,13 @@
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
-  
+
   const toggleMenu = () => {
-    const isMenuOpen =
-      refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
-      refs.openModalBtn.setAttribute('aria-expanded', !isMenuOpen);
+    const isMenuOpen = refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
+    refs.openModalBtn.setAttribute('aria-expanded', !isMenuOpen);
     refs.modal.classList.toggle('is-hidden');
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    document.body.classList.toggle('no-scroll');
   };
 
   refs.openModalBtn.addEventListener('click', toggleMenu);
